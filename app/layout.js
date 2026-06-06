f0d633290f17816c53d481a4c7e98a10f539463a
Dean Chadic <dean@dc3control.com>
2026-06-06 11:44:19 -0500

    fix layout file

--- app/layout.js
+++ app/layout.js
@@ -19,9 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className={`${inter.className} min-h-full flex flex-col`}>
-        {children}
-      </body>
-    </html>
+  <body className={`${inter.className} min-h-full flex flex-col`}>
+    {children}
+  </body>
+</html>
   );
 }
